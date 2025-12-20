f3e20b5839daf3e7dd752861e7e5bfc84b2cc5a8
skalpa13 <pascal.percevault@gmail.com>
2025-12-20 11:26:57 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -2,14 +2,15 @@
 <?php
 
 function bdd_connect() {
-	
+/*	
+  
 	$dsn = 'mysql:dbname=memorygame;host=localhost';
 	$user = 'root';
-	
-/*    $dsn = 'mysql:dbname=acosparla_memorygame;host=mysql-acosparla.alwaysdata.net';
+*/	
+    $dsn = 'mysql:dbname=acosparla_memorygame;host=mysql-acosparla.alwaysdata.net';
     $user = 'acosparla';
     $password = 'marseille';
-	*/
+
 	try {
     	$bdd = new PDO($dsn, $user);
     	$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -25,4 +26,5 @@ function bdd_connect() {
 }
 
 ?>
+
  
